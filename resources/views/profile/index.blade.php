e22693357942afed59663981e92a7ffa564c2e55
bagf <rory@eishgaming.co.za>
2016-12-29 19:21:33 +0200

    Added banner less, ported from inline styles

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,11 +7,12 @@ My Profile
 @section('content')
 
 <div class="pure-g">
-    <div class="pure-u-1" style="position: relative;background: transparent url('{{ url('/images/wave.jpg') }}') no-repeat center center; background-size: cover; min-height: 22em;">
-        <div style="position: absolute; left:0px; bottom: 0px; ; width: 100%;">
-
-            <div style="overflow: hidden; width: 4.5em; text-align: right; margin-right: 0.3em; float: left;color:#fff; font-size: 2.4em; padding: 0.2em;background-color: rgba(0,0,0,0.65);">[ {{ $profile->getTag() }} ]</div>
-            <div style="overflow: hidden; color:#fff; font-size: 2.4em; padding: 0.2em;background: -moz-linear-gradient(left, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0) 100%);">{{ $profile->getDisplayName() }}</div>
+    <div class="pure-u-1">
+        <div class="egn-banner">
+            <div class="egn-banner-content">
+                <div class="egn-tag">[ {{ $profile->getTag() }} ]</div>
+                <div class="egn-primary">{{ $profile->getDisplayName() }}</div>
+            </div>
         </div>
     </div>
 </div>
@@ -24,8 +25,10 @@ My Profile
     </div>
     <div class="pure-u-1-2 egn-standard-text-block">
         <h3>Games</h3>
-        <table><tr><td><img src="{{ url('/images/csgo.png') }}" /></td><td><p>539 hrs<br>210 games</o></td>
-        <table><tr><td><img src="{{ url('/images/dota.png') }}" /></td><td><p>120 hrs<br>12 games</o></td>
+        <table>
+            <tr><td><img src="{{ url('/images/csgo.png') }}" /></td><td><p>539 hrs<br>210 games</o></td></tr>
+            <tr><td><img src="{{ url('/images/dota.png') }}" /></td><td><p>120 hrs<br>12 games</o></td></tr>
+        </table>
     </div>
 </div>
 @stop
