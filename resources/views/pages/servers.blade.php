1975abe88785521ae018e3a1c383362110dca88a
bagf <rory@eishgaming.co.za>
2016-12-31 14:34:46 +0200

    Added full server list link

--- resources/views/pages/servers.blade.php
+++ resources/views/pages/servers.blade.php
@@ -28,7 +28,7 @@ Game Servers
                 We also plan to host a 128 tick rate server if there is demand for it. Join our server by opening console (pressing ~ key) and entering <span class="egn-highlight">connect csco.eishgaming.co.za</span> for example:
             </p>
             <img src="{{ url('/images/csco_connect.png') }}" alt="csco connect">
-            <p>Check out <a href="http://www.moddb.com/mods/counter-strike-classic-offensive">Counter-Strike Classic Offensive's Moddb page</a> for screenshots and download links.</p>
+            <p>Check out <a href="http://www.moddb.com/mods/counter-strike-classic-offensive">Counter-Strike Classic Offensive's Moddb page</a> for screenshots and download links. See <a href="http://steamcommunity.com/groups/classicoffensive/discussions/0/152392786907404561/">this community discussion post</a> for a full list of CS:CO servers.</p>
         </div>
     </div>
 </div>
