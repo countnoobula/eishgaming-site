9a3b3960bc179ae3411f46b4c032c1cc658ef05c
bagf <rory@eishgaming.co.za>
2017-01-02 04:22:09 +0200

    Updated servers links

--- resources/views/pages/servers.blade.php
+++ resources/views/pages/servers.blade.php
@@ -18,7 +18,7 @@ Game Servers
                     <img src="{{ url('/images/csco_icon.png') }}" alt="csco icon">
                 </div>
                 <div class="egn-wrapper">
-                    <a href="http://www.gametracker.com/server_info/    197.96.40.6:27015/" target="_blank">
+                    <a href="http://www.gametracker.com/server_info/197.84.209.49:27015/" target="_blank">
                     <div class="egn-tracker">&nbsp;</div>
                     </a>
                 </div>
@@ -28,7 +28,7 @@ Game Servers
                 We also plan to host a 128 tick rate server if there is demand for it. Join our server by opening console (pressing ~ key) and entering <span class="egn-highlight">connect csco.eishgaming.co.za</span> for example:
             </p>
             <img src="{{ url('/images/csco_connect.png') }}" alt="csco connect">
-            <p>Check out <a href="http://www.moddb.com/mods/counter-strike-classic-offensive">Counter-Strike Classic Offensive's Moddb page</a> for screenshots and download links. See <a href="http://steamcommunity.com/groups/classicoffensive/discussions/0/152392786907404561/">this community discussion post</a> for a full list of CS:CO servers.</p>
+            <p>You can <a href="http://eishgaming.co.za/download/ClassicOffensive_Beta1.1c.zip">download the mod from here</a> or by visiting <a href="http://www.moddb.com/mods/counter-strike-classic-offensive">Counter-Strike Classic Offensive's Moddb page</a>. See <a href="http://steamcommunity.com/groups/classicoffensive/discussions/0/152392786907404561/">this community discussion post</a> for a full list of CS:CO servers.</p>
         </div>
     </div>
 </div>
