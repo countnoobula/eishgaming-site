ed040d2de740eaaa93c5a9eb838773bd7c319ee1
Rory van Heerden <rory@eishgaming.co.za>
2018-07-29 19:03:56 +0200

    Added basic game servers list

--- resources/views/pages/servers.blade.php
+++ resources/views/pages/servers.blade.php
@@ -8,23 +8,113 @@
                     </div>
                 </div>
                 <div class="pure-g">
-                    <div class="pure-u-1 egn-block egn-block__pad">
+                    <div class="pure-u-1-3 egn-block egn-block__pad">
                         <div class="egn-servers">
-                            <div class="egn-game-icon">
-                                <img src="{{ url('/images/csco_icon.png') }}" alt="csco icon">
-                            </div>
-                            <div class="egn-wrapper">
-                                <a href="http://www.gametracker.com/server_info/197.84.209.49:27015/" target="_blank">
-                                <div class="egn-tracker">&nbsp;</div>
-                                </a>
+                            <div class="egn-server egn-server__cstrike">
+                                <h3>Counter-Strike</h3>
+                                <div class="egn-space">&nbsp;</div>
+                                <div class="egn-controls">
+                                    <a href="#" class="cost">100</a>
+                                    <a href="#" class="egn-button">Start server</a>
+                                </div>
                             </div>
                         </div>
-                        <h3>Counter-Strike: Classic Offensive</h3>
-                        <p>We're hosting a Counter-Strike Classic Offensive server in South Africa.
-                            We also plan to host a 128 tick rate server if there is demand for it. Join our server by opening console (pressing ~ key) and entering <span class="egn-highlight">connect csco.eishgaming.co.za</span> for example:
-                        </p>
-                        <img src="{{ url('/images/csco_connect.png') }}" alt="csco connect">
-                        <p>You can <a class="no-ajaxy" href="http://eishgaming.co.za/download/ClassicOffensive_Beta1.1c.zip">download the mod from here</a> or by visiting <a href="http://www.moddb.com/mods/counter-strike-classic-offensive">Counter-Strike Classic Offensive's Moddb page</a>. See <a href="http://steamcommunity.com/groups/classicoffensive/discussions/0/152392786907404561/">this community discussion post</a> for a full list of CS:CO servers.</p>
+                    </div>
+                    <div class="pure-u-1-3 egn-block egn-block__pad">
+                        <div class="egn-servers">
+                            <div class="egn-server egn-server__cscz">
+                                <h3>Counter-Strike Condition Zero</h3>
+                                <div class="egn-space">&nbsp;</div>
+                                <div class="egn-controls">
+                                    <a href="#" class="cost">100</a>
+                                    <a href="#" class="egn-button">Start server</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1-3 egn-block egn-block__pad">
+                        <div class="egn-servers">
+                            <div class="egn-server egn-server__svencoop">
+                                <h3>Half-Life: Sven Coop</h3>
+                                <div class="egn-space">&nbsp;</div>
+                                <div class="egn-controls">
+                                    <a href="#" class="cost">100</a>
+                                    <a href="#" class="egn-button">Start server</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1-3 egn-block egn-block__pad">
+                        <div class="egn-servers">
+                            <div class="egn-server egn-server__hl1">
+                                <h3>Half-Life</h3>
+                                <div class="egn-space">&nbsp;</div>
+                                <div class="egn-controls">
+                                    <a href="#" class="cost">100</a>
+                                    <a href="#" class="egn-button">Start server</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1-3 egn-block egn-block__pad">
+                        <div class="egn-servers">
+                            <div class="egn-server egn-server__hl1dms">
+                                <h3>Half-Life Deathmatch Source</h3>
+                                <div class="egn-space">&nbsp;</div>
+                                <div class="egn-controls">
+                                    <a href="#" class="cost">100</a>
+                                    <a href="#" class="egn-button">Start server</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1-3 egn-block egn-block__pad">
+                        <div class="egn-servers">
+                            <div class="egn-server egn-server__hl1of">
+                                <h3>Half-Life Opposing Force</h3>
+                                <div class="egn-space">&nbsp;</div>
+                                <div class="egn-controls">
+                                    <a href="#" class="cost">100</a>
+                                    <a href="#" class="egn-button">Start server</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1-3 egn-block egn-block__pad">
+                        <div class="egn-servers">
+                            <div class="egn-server egn-server__quake3arena">
+                                <h3>Quake 3 Arena</h3>
+                                <div class="egn-space">&nbsp;</div>
+                                <div class="egn-controls">
+                                    <a href="#" class="cost">100</a>
+                                    <a href="#" class="egn-button">Start server</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1-3 egn-block egn-block__pad">
+                        <div class="egn-servers">
+                            <div class="egn-server egn-server__minecraft">
+                                <h3>Minecraft</h3>
+                                <div class="egn-space">&nbsp;</div>
+                                <div class="egn-controls">
+                                    <a href="#" class="cost">100</a>
+                                    <a href="#" class="egn-button">Start server</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pure-u-1-3 egn-block egn-block__pad">
+                        <div class="egn-servers">
+                            <div class="egn-server egn-server__hl2dm">
+                                <h3>Half-Life 2 Deathmatch</h3>
+                                <div class="egn-space">&nbsp;</div>
+                                <div class="egn-controls">
+                                    <a href="#" class="cost">200</a>
+                                    <a href="#" class="egn-button">Start server</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
